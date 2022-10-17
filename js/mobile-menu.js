b57a5fa9787f1fda162af103f74097204b3f6350
jiseub33 <jiseub33@gmail.com>
2022-10-18 03:25:51 +0900

    update

--- js/mobile-menu.js
+++
@@ -1,8 +0,0 @@
-const toogleBtn = document.querySelector('.header-toogleBtn')
-const menu = document.querySelector('.header-menu')
-const icons = document.querySelector('.header-icons')
-
-toogleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
