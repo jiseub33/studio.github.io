2c8da9c3266e5780871b3e2598ae5b828383a9cd
jiseub33 <jiseub33@gmail.com>
2023-04-27 23:24:18 +0900

    update

--- js/header.js
+++ js/header.js
@@ -1,29 +1,24 @@
 const header = document.querySelector('header');
 const canvas = header.querySelector('.canvas');
-
-window.addEventListener('scroll', function() {
-  if (window.scrollY >= window.innerHeight) {
-    canvas.classList.add('small');
-    header.classList.add('small');
-  } else {
-    canvas.classList.remove('small');
-    header.classList.remove('small');
-  }
-});
-
 const label = document.querySelector('nav.fixheader label');
 const bodyChild = document.querySelector('body > .fixheader');
 
+const remainingHeight = window.innerHeight - header.offsetHeight;
+
 function addBodyClassToLabel() {
   label.classList.add('small');
+  canvas.classList.add('small');
+  header.classList.add('small');
 }
 
 function removeBodyClassFromLabel() {
   label.classList.remove('small');
+  canvas.classList.remove('small');
+  header.classList.remove('small');
 }
 
 window.addEventListener('scroll', function() {
-  if (window.scrollY >= window.innerHeight) {
+  if (window.scrollY >= remainingHeight) {
     addBodyClassToLabel();
   } else {
     removeBodyClassFromLabel();
